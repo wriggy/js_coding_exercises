7f39c821627b49adc17b42c32c49ec3d1ba79e05
wriggyj <67104461+wriggy@users.noreply.github.com>
2020-11-26 03:44:39 +0000

    with forEach

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -2,11 +2,11 @@ function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
   let smallNums = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 1) {
-      smallNums.push(nums[i]);
+  nums.forEach(num => {
+    if (num < 1) {
+      smallNums.push(num);
     }
-  }
+  })
   return smallNums
 }
 
@@ -15,11 +15,11 @@ function findNamesBeginningWith(names, char) {
   if (!char) throw new Error("char is required");
   // Your code here
   let namesBegWith = [];
-  for (let i = 0; i < names.length; i++) {
-    if (names[i].startsWith(char)) {
-      namesBegWith.push(names[i]);
+  names.forEach(name => {
+    if (name.startsWith(char)) {
+      namesBegWith.push(name)
     }
-  }
+  })
   return namesBegWith
 }
 
@@ -27,11 +27,11 @@ function findVerbs(words) {
   if (!words) throw new Error("words is required");
   // Your code here
   let verbs = [];
-  for (let i = 0; i < words.length; i++) {
-    if (words[i].startsWith('to ')) {
-      verbs.push(words[i]);
+  words.forEach(word => {
+    if (word.startsWith('to ')) {
+      verbs.push(word)
     }
-  }
+  })
   return verbs
 }
 
@@ -39,11 +39,11 @@ function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
   let ints = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (Number.isInteger(nums[i])) {
-      ints.push(nums[i]);
+  nums.forEach(num => {
+    if (Number.isInteger(num)) {
+      ints.push(num)
     }
-  }
+  })
   return ints
 }
 
@@ -51,9 +51,9 @@ function getCities(users) {
   if (!users) throw new Error("users is required");
   // Your code here
   let cities = [];
-  for (let i = 0; i < users.length; i++) {
-    cities.push(users[i].data.city.displayName);
-  }
+  users.forEach(user => {
+    cities.push(user.data.city.displayName)
+  })
   return cities
 }
 
@@ -61,11 +61,9 @@ function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
   // Your code here
   let roots = [];
-  for (let i = 0; i < nums.length; i++) {
-    // Math.round() vs toFixed() ??
-    //roots.push(+(Math.pow(nums[i],0.5)).toFixed(2))
-    roots.push(Math.round(Math.pow(nums[i], 0.5) * 100) / 100)
-  }
+  nums.forEach(num => {
+    roots.push(Math.round(Math.pow(num, 0.5) * 100) / 100)
+  })
   return roots
 }
 
@@ -74,12 +72,12 @@ function findSentencesContaining(sentences, str) {
   if (!str) throw new Error("str is required");
   // Your code here
   let retArray = [];
-  let re = new RegExp(str,'i');
-  for (let i = 0; i < sentences.length; i++) {
-    if (sentences[i].match(re)) {
-      retArray.push(sentences[i])
+  let re = new RegExp(str, 'i');
+  sentences.forEach(sentence => {
+    if (sentence.match(re)) {
+      retArray.push(sentence)
     }
-  }
+  })
   return retArray
 }
 
@@ -87,9 +85,8 @@ function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
   // Your code here
   let retArray = [];
-  for (let i=0; i<triangles.length;i++) {
-    retArray.push(Math.max(...triangles[i]))
-  }
+  triangles.forEach(triangle =>
+    retArray.push(Math.max(...triangle)))
   return retArray
 }
 
